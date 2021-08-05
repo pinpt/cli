9a9e6f407e3944a7318275c8b5d5428bee256b80
Jeff Haynie <jhaynie@pinpoint.com>
2021-08-04 20:43:00 -0500

    refactor to work with create-pinpoint-app

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -2,9 +2,9 @@
 
 import { run } from '../lib/runner.mjs';
 import { error } from '../lib/util.mjs';
-import arg from 'arg';
+import { getArgs } from '../lib/args.mjs';
 
-const argsSpec = {
+const optionSpec = {
 	help: { type: Boolean, description: 'This menu :/', alias: 'h', default: false },
 	version: { type: Boolean, description: 'Print the version and exit', alias: 'v', default: false },
 	host: { type: String, description: 'The api host', hidden: true, default: 'api.pinpoint.com' },
@@ -19,38 +19,10 @@ const commandSpec = {
 	signup: { description: 'Signup for Pinpoint' },
 };
 
-const getArgs = () => {
-	let OPTIONS = false;
-	let ARGS = [];
-
-	const _args = {};
-	Object.keys(argsSpec).forEach((key) => {
-		const spec = argsSpec[key];
-		_args[`--${key}`] = spec.type;
-		if (spec.alias) {
-			_args[`-${spec.alias}`] = `--${key}`;
-		}
-	});
-
-	const res = arg(_args, { permissive: true });
-
-	ARGS = res._;
-	OPTIONS = {};
-
-	Object.keys(argsSpec).forEach((key) => {
-		const spec = argsSpec[key];
-		OPTIONS[key] = res[`--${key}`] || res[`-${spec.alias}`] || spec.default;
-	});
-
-	// console.log({ OPTIONS, ARGS, _args, res });
-
-	return [OPTIONS, ARGS];
-};
-
 (async () => {
 	try {
-		const [options, args] = getArgs();
-		await run(args, options, argsSpec, commandSpec);
+		const [options, args] = getArgs(optionSpec);
+		await run({ args, options, optionSpec, commandSpec, meta: import.meta });
 	} catch (ex) {
 		error(ex.message, true);
 	}
